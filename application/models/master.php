6d4832d31e0986060c6002bf5a94069a078f5f93
TianaCy5 <60771489+TianaCy5@users.noreply.github.com>
2021-03-23 21:18:23 +0700

    Update master.php

--- application/models/master.php
+++ application/models/master.php
@@ -4,7 +4,15 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Master extends CI_Model
 {
-  public function getDataCabang(){
-      
-  }
+   public function getallwilayah()
+    {
+        return $this->db->get('master_cabang');
+    }
+    public function cariData()
+    {
+        $keyword = $this->input->post('keyword');
+        $this->db->like('name_cabang', $keyword);
+        $this->db->or_like('id_cabang', $keyword);
+        return $this->db->get('master_cabang')->result_array();
+    }
 }
