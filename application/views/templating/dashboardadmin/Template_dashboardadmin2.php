0c8ded0b86cb1ef5c9ed7d072c4cdf789f727274
TianaCy5 <60771489+TianaCy5@users.noreply.github.com>
2021-04-01 14:40:23 +0700

    Update Template_dashboardadmin2.php

--- application/views/templating/dashboardadmin/Template_dashboardadmin2.php
+++ application/views/templating/dashboardadmin/Template_dashboardadmin2.php
@@ -13,7 +13,7 @@
     <link rel="icon" href="<?php echo base_url() ?>assets/pusatbackend/images/logo/logo.png" type="image/x-icon">
     <link rel="shortcut icon" href="<?php echo base_url() ?>assets/pusatbackend/images/logo/logo.png"
         type="image/x-icon">
-    <title><?= $title ?></title>
+    <title>Senyumdesa</title>
     <!-- Google font-->
     <link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i&amp;display=swap"
         rel="stylesheet">
