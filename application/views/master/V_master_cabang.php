826e8aed58159b08c9dc8143c79e72026b0b7234
TianaCy5 <60771489+TianaCy5@users.noreply.github.com>
2021-04-08 13:08:57 +0700

    Update V_master_cabang.php

--- application/views/master/V_master_cabang.php
+++ application/views/master/V_master_cabang.php
@@ -22,15 +22,62 @@
             <div class="card">
                 <div class="card-header">
                     <h5>Tabel</h5><span>Tabel Master Cabang</span><br>
-                    <a href="<?= base_url('master/tambah'); ?>" class="btn btn-outline-primary" type="button"
-                        data-original-title="btn btn-outline-danger-2x" style="width: 120px;" title=""> Tambah</a>
+                    <!-- Start Modal -->
+                    <button class="btn btn-primary" type="button" data-bs-toggle="modal" data-original-title="test"
+                        data-bs-target="#exampleModal">Tambah Data Cabang</button>
+                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
+                        aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Tambah Data</h5>
+                                    <button class="btn-close" type="button" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div><br>
+                                <!-- isi conten Modal -->
+                                <div class="modal-body">
+
+                                    <form class="needs-validation"
+                                        action="<?php echo base_url('master/prosesTambah') ?>" method="POST">
+                                        <div class="row g-3">
+                                            <div class="col-md-6">
+                                                <label class="form-label" for="nama_cabang">Nama Cabang</label>
+                                                <input class="form-control" name="nama_cabang" id="nama_cabang"
+                                                    type="text" placeholder="Nama Cabang" required="">
+                                            </div>
+                                            <div class="col-md-6">
+                                                <label class="form-label" for="status_cabang">Status cabang</label>
+                                                <select class="form-select" name="status_cabang" id="status_cabang"
+                                                    required="">
+                                                    <option selected="" disabled="" value="">Pilih Status</option>
+                                                    <option value="active">Aktif</option>
+                                                    <option value="inactive">Tidak Aktif</option>
+                                                </select>
+                                                <div class="invalid-feedback">status cabang.</div>
+                                            </div>
+                                        </div>
+
+                                </div>
+                                <div class="modal-footer">
+                                    <button class="btn btn-primary" type="submit">Save Data</button>
+                                    <input class="btn btn-warning" type="reset" value="Reset">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end Modal-->
+
                 </div>
+
 
                 <div class="card-body">
                     <div class="table-responsive">
+
                         <table class="display" id="basic-1" width="100%" cellspacing="0">
                             <thead style=" text-align: center;">
                                 <tr>
+                                    <th width="5%"><button type="button" name="delete" id="delete"
+                                            class="btn btn-danger btn-xs">Delete</button></th>
                                     <th>No</th>
                                     <th>Nama Cabang</th>
                                     <th>Status Cabang</th>
@@ -42,6 +89,8 @@
                                 <?php $no = 1;
                                 foreach ($master_cabang->result_array() as $msr) { ?>
                                 <tr>
+                                    <td><input type="checkbox" class="delete_checkbox"
+                                            value="'<?= $msr["id_cabang"]; ?>'" /></td>
                                     <td> <?= $no++; ?></td>
                                     <td><?= $msr["name_cabang"]; ?></td>
                                     <td><?= $msr["status_cabang"]; ?></td>
@@ -86,10 +135,13 @@
                                     ?>
                             </tbody>
                         </table>
+
+
                     </div>
                 </div>
             </div>
         </div>
+
 
 
     </div>
