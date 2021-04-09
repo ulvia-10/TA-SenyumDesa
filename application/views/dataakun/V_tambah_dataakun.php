fc61de23f8667bc3a6b849759be3630f1e7e7b0a
TianaCy5 <60771489+TianaCy5@users.noreply.github.com>
2021-04-09 21:49:02 +0700

    Update V_tambah_dataakun.php

--- application/views/dataakun/V_tambah_dataakun.php
+++ application/views/dataakun/V_tambah_dataakun.php
@@ -1,207 +1,189 @@
+<div class="container-fluid">
+    <div class="page-title">
+        <div class="row">
+            <div class="col-6">
+                <h3>Data Akun</h3>
+            </div>
+            <div class="col-6">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index.html"> <i data-feather="home"></i></a></li>
+                    <li class="breadcrumb-item">Data Tables</li>
+                    <li class="breadcrumb-item active">Tambah Data Akun</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Container-fluid starts-->
+
+
 <div class="card">
-	<div class="card-header">
-		<h5>Tambah Akun Baru</h5>
-	</div>
-	<form class="form theme-form" action="<?php echo base_url('data_akun/prosesTambah') ?>" method="POST" enctype="multipart/form-data">
-		<div class="card-body">
-			<div class="row">
-				<div class="col">
-					<div class="mb-2 row">
-						<label class="col-sm-3 col-form-label">Name</label>
-						<div class="col-sm-9">
-							<input class="form-control" type="text" placeholder="Type full name" name="full_name"
-								id="full_name">
-						</div>
-					</div>
-					<div class="mb-3 row">
-						<label class="col-sm-3 col-form-label">Username</label>
-						<div class="col-sm-9">
-							<input class="form-control" type="text" placeholder="Type username" name="username"
-								id="username">
-						</div>
-					</div>
-					<!-- <div class="mb-3 row">
-						<label class="col-sm-3 col-form-label">Email</label>
-						<div class="col-sm-9">
-							<input class="form-control" id="exampleInputEmail1" type="email" name="email" id="email"
-								placeholder="Masukkan email yang valid">
-						</div>
-					</div> -->
+    <div class="card-header">
+        <h5>Tambah Akun Baru</h5>
+    </div>
+    <form class="form theme-form" action="<?php echo base_url('data_akun/prosesTambah') ?>" method="POST"
+        enctype="multipart/form-data">
+        <div class="card-body">
+            <div class="row">
+                <div class="col">
+                    <div class="mb-2 row">
+                        <label class="col-sm-3 col-form-label">Name</label>
+                        <div class="col-sm-9">
+                            <input class="form-control" type="text" placeholder="Isi Nama" name="full_name"
+                                id="full_name" required="Nama harap DiIsi">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-sm-3 col-form-label">Username</label>
+                        <div class="col-sm-9">
+                            <input class="form-control" type="text" placeholder="Isi username" name="username"
+                                id="username" required="Username harap DiIsi">
+                        </div>
+                    </div>
 
-					<div class="mb-3 row">
-						<label class="col-sm-3 col-form-label">Password</label>
-						<div class="col-sm-9">
-							<input class="form-control" type="password" placeholder="Input max length 6" maxlength="6"
-								name="password" id="password">
-						</div>
-					</div>
+                    <div class="mb-3 row">
+                        <label class="col-sm-3 col-form-label">Password</label>
+                        <div class="col-sm-9">
+                            <input class="form-control" type="password" placeholder="Input max length 6" maxlength="6"
+                                name="password" id="password">
+                        </div>
+                    </div>
 
-					<!-- <div class="mb-3 row">
-						<label class="col-sm-3 col-form-label">Telephone</label>
-						<div class="col-sm-9">
-							<input class="form-control m-input digits" type="tel" placeholder="91-(999)-999-999"
-								name="telp" id="telp" maxlength="12">
-						</div>
-					</div> -->
-					<div class="mb-2 row">
-						<label class="col-sm-3 col-form-label">Tempat Lahir</label>
-						<div class="col-sm-9">
-							<input class="form-control" type="text" placeholder="Type your born place"
-								name="tempat_lahir" id="tempat_lahir">
-						</div>
-					</div>
 
-					<div class="mb-3 row">
-						<label class="col-sm-3 col-form-label">Date</label>
-						<div class="col-sm-9">
-							<input class="form-control digits" type="date" value="2018-01-01" name="tanggal_lahir"
-								id="tanggal_lahir">
-						</div>
-					</div>
-					<div class="mb-2 row">
-						<label class="col-sm-3 col-form-label">Asal</label>
-						<div class="col-sm-9">
-							<input class="form-control" type="text" placeholder="Type asal kampus/instansi/kerja"
-								name="asal" id="asal">
-						</div>
-					</div>
-					<div class="row">
-						<label class="col-sm-3 col-form-label">Gender</label>
-						<div class="col-sm-9">
-							<div class="m-checkbox-inline custom-radio-ml">
-								<div class="form-check form-check-inline radio radio-primary">
-									<input class="form-check-input" id="radioinline4" type="radio" name="gender" id="gender"
-										value="P">
-									<label class="form-check-label mb-0" for="radioinline4"><span class="digits">
-											Female</span></label>
-								</div>
-								<div class="form-check form-check-inline radio radio-primary">
-									<input class="form-check-input" id="radioinline5" type="radio" name="gender" id="gender"
-										value="L">
-									<label class="form-check-label mb-0" for="radioinline5"><span class="digits">
-											Male</span></label>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col">
-							<div class="mb-3 row">
-								<label class="col-sm-3 col-form-label">Pas Foto</label>
-								<div class="col-sm-9">
-									<input class="form-control" type="file" name="photo" id="photo">
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="row">
-						<label class="col-sm-3 col-form-label">Level</label>
-						<div class="col-sm-9">
-							<div class="m-checkbox-inline custom-radio-ml">
-								<div class="form-check form-check-inline radio radio-primary">
-									<input class="form-check-input" id="radioinline1" type="radio" name="level" id="level"
-										value="pusat">
-									<label class="form-check-label mb-0" for="radioinline1">Option<span class="digits">
-											Admin Pusat</span></label>
-								</div>
-								<div class="form-check form-check-inline radio radio-primary">
-									<input class="form-check-input" id="radioinline2" type="radio" name="level" id="level"
-										value="korwil">
-									<label class="form-check-label mb-0" for="radioinline2">Option<span class="digits">
-											Admin Korwil</span></label>
-								</div>
-								<div class="form-check form-check-inline radio radio-primary">
-									<input class="form-check-input" id="radioinline3" type="radio" name="level" id="level"
-										value="anggota">
-									<label class="form-check-label mb-0" for="radioinline3">Option<span class="digits">
-											Anggota</span></label>
-								</div>
-							</div>
-						</div>
-					</div>
+                    <div class="mb-2 row">
+                        <label class="col-sm-3 col-form-label">Tempat Lahir</label>
+                        <div class="col-sm-9">
+                            <input class="form-control" type="text" placeholder="Isi tempat lahir" name="tempat_lahir"
+                                id="tempat_lahir" required="Tempat Lahir harap DiIsi">
+                        </div>
+                    </div>
 
-					<div class="form-group">
+                    <div class="mb-3 row">
+                        <label class="col-sm-3 col-form-label">Date</label>
+                        <div class="col-sm-9">
+                            <input class="form-control digits" type="date" value="2018-01-01" name="tanggal_lahir"
+                                id="tanggal_lahir" required="Tanggal harap DiIsi">
+                        </div>
+                    </div>
+                    <div class="mb-2 row">
+                        <label class="col-sm-3 col-form-label">Asal</label>
+                        <div class="col-sm-9">
+                            <input class="form-control" type="text" placeholder="Isi asal kampus/instansi/kerja"
+                                name="asal" id="asal" required="asal harap DiIsi">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <label class="col-sm-3 col-form-label">Gender</label>
+                        <div class="col-sm-9">
+                            <div class="m-checkbox-inline custom-radio-ml">
+                                <div class="form-check form-check-inline radio radio-primary">
+                                    <input class="form-check-input" id="radioinline4" type="radio" name="gender"
+                                        id="gender" value="P" required="Gender harap DiIsi">
+                                    <label class="form-check-label mb-0" for="radioinline4"><span class="digits">
+                                            Perempuan</span></label>
+                                </div>
+                                <div class="form-check form-check-inline radio radio-primary">
+                                    <input class="form-check-input" id="radioinline5" type="radio" name="gender"
+                                        id="gender" value="L">
+                                    <label class="form-check-label mb-0" for="radioinline5"><span class="digits">
+                                            Laki</span></label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="mb-3 row">
+                                <label class="col-sm-3 col-form-label" for="userfile">Pas Foto</label>
+                                <div class="col-sm-9">
+                                    <input class="form-control" type="file" name="userfile">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <label class="col-sm-3 col-form-label">Level</label>
+                        <div class="col-sm-9">
+                            <div class="m-checkbox-inline custom-radio-ml">
+                                <div class="form-check form-check-inline radio radio-primary">
+                                    <input class="form-check-input" id="radioinline1" type="radio" name="level"
+                                        id="level" value="pusat" required="Level harap DiIsi">
+                                    <label class="form-check-label mb-0" for="radioinline1"><span class="digits">
+                                            Admin Pusat</span></label>
+                                </div>
+                                <div class="form-check form-check-inline radio radio-primary">
+                                    <input class="form-check-input" id="radioinline2" type="radio" name="level"
+                                        id="level" value="korwil" required="Level harap DiIsi">
+                                    <label class="form-check-label mb-0" for="radioinline2"><span class="digits">
+                                            Admin Korwil</span></label>
+                                </div>
+                                <div class="form-check form-check-inline radio radio-primary">
+                                    <input class="form-check-input" id="radioinline3" type="radio" name="level"
+                                        id="level" value="anggota" required="Level harap DiIsi">
+                                    <label class="form-check-label mb-0" for="radioinline3"><span class="digits">
+                                            Anggota</span></label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-						<div class="row" id="element-wilayah">
-							<div class="mb-2 row">
-								<label class="col-sm-3 col-form-label">Wilayah</label>
-								<div class="col-sm-9">
-									<select name="name_cabang" class="form-select digits" id="">
-										<option value="name_cabang">-- Pilih salah satu --</option>
+                    <div class="form-group">
 
-										<?php
-										
-										
-											foreach ( $data_master->result_array() AS $cabang) {
+                        <div class="row" id="element-wilayah">
+                            <div class="mb-2 row">
+                                <label class="col-sm-3 col-form-label">Wilayah</label>
+                                <div class="col-sm-9">
+                                    <select name="name_cabang" class="form-select digits" id=""
+                                        required="wilayah harap DiIsi">
+                                        <option value="name_cabang">-- Pilih salah satu --</option>
 
-												echo '<option value="'.$cabang['id_cabang'].'">'.$cabang['name_cabang'].'</option>';
-											}
-										?>
-									</select>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="row">
+                                        <?php
 
-						<label class="col-sm-3 col-form-label">Status Account</label>
-						<div class="col-sm-9">
-							<div class="m-checkbox-inline custom-radio-ml">
-								<div class="form-check form-check-inline radio radio-primary">
-									<input class="form-check-input" id="radioinline7" type="radio" name="status_account" id="status_account"
-										value="active">
-									<label class="form-check-label mb-0" for="radioinline7"><span class="digits">
-											Active</span></label>
-								</div>
-								<div class="form-check form-check-inline radio radio-primary">
-									<input class="form-check-input" id="radioinline8" type="radio"  name="status_account" id="status_account"
-										value="inactive">
-									<label class="form-check-label mb-0" for="radioinline8"><span class="digits">
-											Inactive</span></label>
-								</div>
 
-							</div>
-						</div>
-					</div>
-					<!-- <div class="row">
-						<label class="col-sm-3 col-form-label">Address</label>
-						<div class="col-sm-9">
-							<textarea class="form-control" rows="5" cols="5" placeholder="Type Address" name="address"
-								id="address"></textarea>
-						</div>
-					</div> -->
-				</div>
-			</div>
-		</div>
-		<div class="card-footer">
-			<div class="col-sm-9 offset-sm-3">
-				<button class="btn btn-primary" type="submit">Tambah</button>
-				<input class="btn btn-light" type="reset" value="Cancel">
-			</div>
-		</div>
-	</form>
+                                        foreach ($data_master->result_array() as $cabang) {
+
+                                            echo '<option value="' . $cabang['id_cabang'] . '">' . $cabang['name_cabang'] . '</option>';
+                                        }
+                                        ?>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+
+                        <label class="col-sm-3 col-form-label">Status Account</label>
+                        <div class="col-sm-9">
+                            <div class="m-checkbox-inline custom-radio-ml">
+                                <div class="form-check form-check-inline radio radio-primary">
+                                    <input class="form-check-input" id="radioinline7" type="radio" name="status_account"
+                                        id="status_account" value="active" required="status harap DiIsi">
+                                    <label class="form-check-label mb-0" for="radioinline7"><span class="digits">
+                                            Active</span></label>
+                                </div>
+                                <div class="form-check form-check-inline radio radio-primary">
+                                    <input class="form-check-input" id="radioinline8" type="radio" name="status_account"
+                                        id="status_account" value="inactive" required="status akun harap DiIsi">
+                                    <label class="form-check-label mb-0" for="radioinline8"><span class="digits">
+                                            Inactive</span></label>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="card-footer">
+            <div class="col-sm-9 offset-sm-3">
+                <button class="btn btn-primary" type="submit">Tambah</button>
+                <input class="btn btn-warning" type="reset" value="Reset">
+                <a href="<?= base_url('Data_akun'); ?>" class="btn btn-light" type="submit">Cancel</a>
+            </div>
+        </div>
+    </form>
 </div>
 
 
 <script>
-
-
-	let elementWilayah = $('#element-wilayah');
-
-	// sembunyikan
-	elementWilayah.hide();
-
-	// perintah event on click
-	$('input[name="level"]').change(function () {
-
-
-		if (this.value == "pusat") {
-
-			elementWilayah.fadeOut();
-		} else {
-
-			elementWilayah.fadeIn();
-		}
-	});
-
-</script>
